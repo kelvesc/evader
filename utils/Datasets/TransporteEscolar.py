16cc4add98de941de24c66dc63e0fc145368aff2
kelvesc <kelves.nunes@ufrpe.br>
2024-12-07 22:54:27 -0300

    (chore): made changes to how Estudantes* and Transport datasets are read .

--- utils/Datasets/TransporteEscolar.py
+++ utils/Datasets/TransporteEscolar.py
@@ -3,5 +3,5 @@ from pandas import read_csv, DataFrame
 path_dataset = 'utils/Datasets/data/transporte.csv'
 
 def load_data_transporte(path_to_data=path_dataset):
-    return DataFrame(read_csv(path_to_data))
+    return DataFrame(read_csv(path_to_data, sep=";"))
 
